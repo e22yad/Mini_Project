773016b3e3bccb596e9812e142cc1e2ed7586487
STRiDER047 <147204034+STRiDER047@users.noreply.github.com>
2023-10-26 23:01:11 +0300

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,47 +1,17 @@
 <?php
-//function
 
-function check_login($conn)
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$DBname = "DB";
+
+
+
+
+if(!$conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$DBname))
 {
 
-	if(isset($_SESSION['user_id']))
-	{
-
-		$id = $_SESSION['user_id'];
-		$query = "select * from users where user_id = '$id' limit 1";
-
-		$result = mysqli_query($conn,$query);
-		if($result && mysqli_num_rows($result) > 0)
-		{
-
-			$user_data = mysqli_fetch_assoc($result);
-			return $user_data;
-		}
-	}
-
-	//redirect to login
-	header("Location: rent.html");
-	//
-
+	die("failed to connect!");
 }
 
-function random_num($length)
-{
-
-	$text = "";
-	if($length < 3)
-	{
-		$length = 3;
-	}
-
-	$len = rand(3,$length);
-
-	for ($i=0; $i < $len; $i++) { 
-		# code...
-
-		$text .= rand(0,9);
-	}
-
-	return $text;
-}
 ?>
