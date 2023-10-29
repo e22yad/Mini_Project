3a1f27e1586e930a81969773cbb81be83f393437
STRiDER047 <147204034+STRiDER047@users.noreply.github.com>
2023-10-29 19:20:25 +0300

    Update signUP.php

--- signUP.php
+++ signUP.php
@@ -7,21 +7,23 @@ include("function.php");
 		//something was posted
 		$user_name = $_POST['user_name2'];
 		$password = $_POST['password2'];
+		$email = $_POST['email2'];
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		if(!empty($user_name) && !empty($password))
 		{
 
 			//save to database
+			
 			$user_id = random_num(9);
-			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
-
+			$query = "insert into users (user_id,user_name,email2,password) values ('$user_id','$user_name','$email','$password')";
+            
 			mysqli_query($conn, $query);
 
 			echo "You Have Ben Registered Now Go and log-in Hear--><a href='carR.html'>Main Page</a>";
 			
 		}else
 		{
-			
+			echo "Wrong";
 		}
 	
 
